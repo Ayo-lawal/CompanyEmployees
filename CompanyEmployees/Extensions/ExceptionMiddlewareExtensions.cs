3e234e6577e14b6aa1d38c3211f0825d39359a93
Mujeeb Ayobami Lawal <ayolawal247@outlook.com>
2023-02-14 10:47:32 +0100

    updated

--- CompanyEmployees/Extensions/ExceptionMiddlewareExtensions.cs
+++ CompanyEmployees/Extensions/ExceptionMiddlewareExtensions.cs
@@ -1,6 +1,34 @@
-﻿namespace CompanyEmployees.Extensions
+﻿
+using Entities.ErrorModel;
+using Microsoft.AspNetCore.Diagnostics;
+using System.Net;
+
+namespace CompanyEmployees.Extensions
 {
-    public class ExceptionMiddlewareExtensions
+    public static class ExceptionMiddlewareExtensions
     {
+        public static void ConfigureExceptionHandler(this WebApplication app, ILoggerManager logger)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeature != null) 
+                    {
+                        logger.LogError($"Something went wrong: {contextFeature.Error}");   
+
+                        await context.Response.WriteAsync(new ErrorDetails()
+                        {
+                            StatusCode = context.Response.StatusCode,
+                            Message = "Internal Server Error.",
+                        }.ToString());
+                    }
+                });
+            });
+        }
     }
 }
--- Entities/ErrorModel/ErrorDetails.cs
+++ Entities/ErrorModel/ErrorDetails.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.Json;
 
 namespace Entities.ErrorModel
 {
-    internal class ErrorDetails
+    public class ErrorDetails
     {
+        public int StatusCode { get; set; }
+        public string? Message { get; set; }
+        public override string ToString() => JsonSerializer.Serialize(this);
     }
 }
